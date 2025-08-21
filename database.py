dcfce5f635db22c7e03083dcb9001867fe0e7d5a
Ankitha <ankithaachar16@gmail.com>
2025-08-21 14:40:09 +0530

    task 3

--- database.py
+++ database.py
@@ -1,7 +1,37 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@db:5432/todo_db"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# use environment variable so it's flexible
 DATABASE_URL = "postgresql://todo_user:password@db:5432/todo_db"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- models.py
+++ models.py
@@ -1,11 +1,47 @@
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+# Base = declarative_base()
+
+# class User(Base):
+#     __tablename__="users"
+
+#     id = Column(Integer,primary_key=True,index=True)
+#     username = Column(String,unique=True,index=True,nullable=False)
+#     email = Column(String,unique=True,index=True,nullable=False)
+#     hashed_password = Column(String,nullable=False)
+#     is_active = Column(Boolean,default=True)
+#     todos = relationship("todo",back_populates="owner")
+
+# class Todo(Base):
+#     __tablename__ = "todos"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     task = Column(String, nullable=False)
+#     done = Column(Boolean, default=False)
+#     owner_id = Column(Integer,ForeignKey("user.id"))
+
+#     owner = relationship("User",back_populates="todos")
+
+
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    todos = relationship("Todo", back_populates="owner")
 
 class Todo(Base):
     __tablename__ = "todos"
-
     id = Column(Integer, primary_key=True, index=True)
     task = Column(String, nullable=False)
     done = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="todos")
